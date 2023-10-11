d50f89f479a337afe2c66a51601d8cb5ce0cab02
Meehmeet <mehmet.sahin@student.htldornbirn.at>
2023-10-11 09:21:43 +0200

    readme erstellt

--- src/at/mes/basic/DiceGame.java
+++ src/at/mes/basic/DiceGame.java
@@ -1,6 +1,7 @@
 package at.mes.basic;
 
 import java.util.Random;
+import java.util.Scanner;
 public class DiceGame {
     public static void main(String[] args) {
         Random random = new Random();
@@ -8,31 +9,44 @@ public class DiceGame {
         int WinsPlayer = 0;
         int WinsComputer = 0;
         int Draws = 0;
+        int selection = 0;
+        Scanner scanner = new Scanner(System.in);
+        char cWiederholung = 'b';
 
-        System.out.println("--------------DICE GAME--------------");
-        for(int i = 0; i < 6; i++)
-        {
-            int randomPlayer = random.nextInt(6 ) + 1;
-            int randomComputer = random.nextInt(6 ) + 1;
+        do {
+            cWiederholung = 'b';
+            System.out.println("--------------DICE GAME--------------");
+            System.out.println("Menue:");
+            System.out.println("\t Würfeln (1)");
+            System.out.println("\t Stopp (2)");
 
-            if(randomPlayer > randomComputer)
-            {
-                WinsPlayer++;
+            selection = scanner.nextInt();
+
+            if (selection == 1) {
+                for (int i = 0; i < 6; i++) {
+                    int randomPlayer = random.nextInt(6) + 1;
+                    int randomComputer = random.nextInt(6) + 1;
+
+                    if (randomPlayer > randomComputer) {
+                        WinsPlayer++;
+                    } else if (randomComputer > randomPlayer) {
+                        WinsComputer++;
+                    } else {
+                        Draws++;
+                    }
+                    System.out.println("\n Roll " + (i + 1));
+                    System.out.println("Player: " + randomPlayer + "\nComputer: " + randomComputer);
+                }
+
+                System.out.println("\n--------------END--------------");
+                System.out.println("Player Wins: " + WinsPlayer + "\nComputer Wins: " + WinsComputer + "\nDraws: " + Draws);
+
+                System.out.println("\n Wollen Sie nocheinmal spielen (j) oder abbrechen (n)?");
+                cWiederholung = scanner.next().charAt(0);
             }
-
-            else if (randomComputer > randomPlayer)
-            {
-                WinsComputer++;
-            }
-
             else{
-                Draws++;
+            System.out.println("Ende");
             }
-            System.out.println("\n Roll " + (i + 1));
-            System.out.println("Player: " + randomPlayer + "\nComputer: " + randomComputer);
-        }
-
-        System.out.println("\n--------------END--------------");
-        System.out.println("Player Wins: " + WinsPlayer + "\nComputer Wins: " + WinsComputer + "\nDraws: " + Draws);
+    }while(cWiederholung == 'j');
     }
 }
