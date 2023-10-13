b018296ca6687180e93d4d4fa5ffd6321d6a5fdf
Meehmeet <mehmet.sahin@student.htldornbirn.at>
2023-10-13 09:35:11 +0200

    readme erstellt

--- src/at/mes/basic/StringHelper.java
+++ src/at/mes/basic/StringHelper.java
@@ -1,7 +1,51 @@
 package at.mes.basic;
+import java.util.Scanner;
 
 public class StringHelper {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String Palindrome = "";
+        System.out.println("Gebe dein Wort, bei dem du wissen willst ob es Palindrome ist");
+        Palindrome = scanner.nextLine();
+
+
+
+
+        String text = "";
+        char search = 'a';
+
+        System.out.println("Gebe deinen Text ein bei dem du die Buchstaben zählen willst");
+        text = scanner.nextLine();
+
+        System.out.println("Gebe den Buchstaben ein den du zählen willst");
+        search = scanner.next().charAt(0);
+
+        int result = countLetters(text,search);
+        System.out.println("Anzahl des Buchstaben: " + result);
+
+
+
+
+    }
+    public static boolean isPalindrome(String Palindrome) {
+        boolean Ausgabe = true;
+        char[] Palindrome
+
+        return Ausgabe;
+    }
+
+    public static int countLetters(String text, char search) {
+        char[] textArray = text.toCharArray();
+        int count = 0;
+
+        for(int i = 0; i < textArray.length; i++){
+            if(textArray[i]==search) {
+                count++;
+            }
+        }
+        return count;
     }
 }
