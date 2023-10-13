dba5f76cdb43d1db9991dbed046d40fe26e2066f
Meehmeet <mehmet.sahin@student.htldornbirn.at>
2023-10-13 09:02:46 +0200

    readme erstellt

--- src/at/mes/basic/Caesar.java
+++ src/at/mes/basic/Caesar.java
@@ -1,10 +1,22 @@
 package at.mes.basic;
 import java.awt.im.spi.InputMethodDescriptor;
 import java.io.CharConversionException;
+import java.util.Scanner;
 
 public class Caesar {
     public static void main(String[] args) {
-        String result = encrypt(1,"Hallo");
+        int selection = 0;
+        String selection2 = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Gebe deinen String ein den du verschluesseln willst");
+        selection2 = scanner.nextLine();
+
+        System.out.println("\nGebe ein um wie viele Zeilen sich der String verschieben soll");
+        selection = scanner.nextInt();
+
+
+        String result = encrypt(selection, selection2);
         System.out.println(result);
     }
 
---
+++ src/at/mes/basic/StringHelper.java
@@ -0,0 +1,7 @@
+package at.mes.basic;
+
+public class StringHelper {
+    public static void main(String[] args) {
+
+    }
+}
