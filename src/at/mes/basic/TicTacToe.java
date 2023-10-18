07482a56f92197ffead798e6ef91b22ffe296849
Meehmeet <mehmet.sahin@student.htldornbirn.at>
2023-10-18 09:25:30 +0200

    readme erstellt

--- src/at/mes/basic/TicTacToe.java
+++ src/at/mes/basic/TicTacToe.java
@@ -1,4 +1,9 @@
 package at.mes.basic;
-
+import java.util.Scanner;
 public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    public static int Feld()
 }
