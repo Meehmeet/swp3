af9bd7aba2b026e1a9b362a70b975482eb2e2b27
Meehmeet <mehmet.sahin@student.htldornbirn.at>
2023-10-25 09:11:46 +0200

    readme erstellt

--- src/at/mes/basic/TicTacToe.java
+++ src/at/mes/basic/TicTacToe.java
@@ -18,57 +18,107 @@ public class TicTacToe {
         }
 
 
-        int[] ZahlArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] moveArray = {0,0,0,0,0,0,0,0,0};
-        int iZahl = -1;
+        int iZahl = 0;
 
         do {
-            iZahl = iZahl + 1;
-            moveArray[iZahl] = Auswahl();
-            gewonnen = Feld(ZahlArray, moveArray, spielfeld);
+            Feld(spielfeld);
+            if(iZahl % 2 == 0) {
+                int move = Auswahl(spielfeld, 1);
+                int Zeile = (move - 1) / 3;
+                int Spalte = (move - 1) % 3;
 
-            iZahl = iZahl + 1;
-            moveArray[iZahl] = Auswahl2();
-            gewonnen = Feld(ZahlArray, moveArray, spielfeld);
+                if(spielfeld[Zeile][Spalte] == ' ') {
+                    spielfeld[Zeile][Spalte] = 'X';
+                }
+                else{
+                    System.out.println("Dieses Feld ist bereits belegt. Wähle ein neues.");
+                    continue;
+                }
+            }
+            else{
+                int move = Auswahl(spielfeld, 2);
+                int Zeile = (move - 1) / 3;
+                int Spalte = (move - 1) % 3;
 
-        } while (gewonnen = false);
+                if(spielfeld[Zeile][Spalte] == ' ') {
+                    spielfeld[Zeile][Spalte] = 'O';
+                }
+                else{
+                    System.out.println("Dieses Feld ist bereits belegt. Wähle ein neues.");
+                    continue;
+                }
+            }
+            iZahl++;
+
+            gewonnen = gewonnencheck(spielfeld);
+            if(gewonnen) {
+                Feld(spielfeld);
+                if(iZahl % 2 == 0) {
+                    System.out.println("Spieler 2 hat gewonnen.");
+                } else {
+                    System.out.println("Spieler 1 hat gewonnen.");
+                }
+            }
+        }while(!gewonnen && iZahl < 9);
+
+        if(!gewonnen) {
+            System.out.println("Unentschieden, es hat keiner gewonnen.");
+        }
     }
 
-    public static int Auswahl() {
+    public static int Auswahl(char[][] spielfeld, int spieler) {
         Scanner scanner = new Scanner(System.in);
         int move = 0;
-        System.out.println("Spieler 1, gebe ein wo du dein x setzen willst (1-9) ");
-        move = scanner.nextInt();
-
+        do {
+            System.out.println("Spieler " + spieler + ", gebe ein wo du deinen Punkt setzen willst (1-9) ");
+            move = scanner.nextInt();
+        }while(move < 1 || move > 9 || !moeglichmove(spielfeld, move));
         return move;
     }
 
-    public static int Auswahl2() {
-        Scanner scanner = new Scanner(System.in);
-        int move2 = 0;
-        System.out.println("Spieler 2, gebe ein wo du dein o setzen willst (1-9) ");
-        move2 = scanner.nextInt();
-
-        return move2;
-    }
-
-    public static boolean Feld(int[] Array, int[] moveArray, char[][] spielfeld) {
-        boolean ausgabe = false;
+    public static void Feld(char[][] spielfeld) {
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(spielfeld[i][j]);
-                if (j < 2) {
-                    System.out.print(" | ");
-                }
+                System.out.print("| " + spielfeld[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("|");
+
             if (i < 2) {
-                System.out.println("---------");
+                System.out.println("-------------");
             }
         }
 
-        return ausgabe;
     }
 
+    public static boolean gewonnencheck(char[][] spielfeld) {
+        for (int Zeile = 0; Zeile < 3; Zeile++) {
+            if (spielfeld[Zeile][0] == spielfeld[Zeile][1] &&
+                    spielfeld[Zeile][1] == spielfeld[Zeile][2] &&
+                    spielfeld[Zeile][0] != ' ') {
+                return true;
+            }
+        }
+
+        for (int Spalte = 0; Spalte < 3; Spalte++) {
+            if (spielfeld[0][Spalte] == spielfeld[1][Spalte] &&
+                    spielfeld[1][Spalte] == spielfeld[2][Spalte] &&
+                    spielfeld[0][Spalte] != ' ') {
+                return true;
+            }
+        }
+
+        if ((spielfeld[0][0] == spielfeld[1][1] && spielfeld[1][1] == spielfeld[2][2] && spielfeld[0][0] != ' ') ||
+                (spielfeld[0][2] == spielfeld[1][1] && spielfeld[1][1] == spielfeld[2][0] && spielfeld[0][2] != ' ')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean moeglichmove(char[][] spielfeld, int move) {
+        int Zeile = (move - 1) / 3;
+        int Spalte = (move - 1) % 3;
+        return spielfeld[Zeile][Spalte] == ' ';
+    }
 }
